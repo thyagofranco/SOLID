35cc7a506cd1717fc19695c6d2bfbf4df6d626b2
Thyago Franco <thyago.franco@matera.com>
2021-04-28 14:39:19 -0300

    Naive approach to attend business requirements.

--- ocp/before/GlobomanticsHR/src/main/java/hr/taxes/TaxCalculator.java
+++ ocp/before/GlobomanticsHR/src/main/java/hr/taxes/TaxCalculator.java
@@ -1,17 +1,36 @@
 package hr.taxes;
 
 import hr.personnel.Employee;
+import hr.personnel.FullTimeEmployee;
+import hr.personnel.Intern;
+import hr.personnel.PartTimeEmployee;
 
 public class TaxCalculator {
     private final int RETIREMENT_TAX_PERCENTAGE = 10;
     private final int INCOME_TAX_PERCENTAGE = 16;
     private final int BASE_HEALTH_INSURANCE = 100;
 
+    public double calculate(Employee employee){
+        int monthyIncome = employee.getMonthlyIncome();
 
-    public double calculate(Employee employee) {
-        return BASE_HEALTH_INSURANCE +
-                (employee.getMonthlyIncome() * RETIREMENT_TAX_PERCENTAGE) / 100 +
-                (employee.getMonthlyIncome() * INCOME_TAX_PERCENTAGE) / 100;
+        if(employee instanceof FullTimeEmployee){
+            return BASE_HEALTH_INSURANCE +
+                    (monthyIncome * RETIREMENT_TAX_PERCENTAGE) / 100 +
+                    (monthyIncome * INCOME_TAX_PERCENTAGE) / 100;
+        }
+
+        if(employee instanceof PartTimeEmployee) {
+            return BASE_HEALTH_INSURANCE +
+                    (monthyIncome * 10) / 100 +
+                    (monthyIncome * INCOME_TAX_PERCENTAGE) / 100;
+        }
+
+        if(employee instanceof Intern) {
+            if(monthyIncome < 350) return 0;
+            else return (monthyIncome + INCOME_TAX_PERCENTAGE) / 100;
+        }
+
+        return 0;
 
     }
 }
